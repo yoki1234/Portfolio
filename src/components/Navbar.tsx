831b61f293b7182eabde70f87297cb2f3bac070c
Akshay_Ashok <akshayashokdj@gmail.com>
2026-08-16 17:18:53 +0100

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+  return (
+    <nav className="flex gap-6 p-4 bg-slate-900 text-white">
+      <Link to="/" className="hover:text-teal-400">Home</Link>
+    </nav>
+  )
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold text-slate-900">Welcome to My Portfolio</h1>
+    </div>
+  )
+}
